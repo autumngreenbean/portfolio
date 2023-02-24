6a3eba9698f11007c13970996b1acd7110a50eb0
Autumn Jing Green <67246658+autumngreenbean@users.noreply.github.com>
2023-02-24 11:52:46 -0800

    Update main.js
    chatgpt debug

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-// import './style.css'
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1,1000);
 
+const canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+
 const renderer = new THREE.WebGL1Renderer({
-canvas: document.querySelector('#bg'),
-  
-})
+  canvas: canvas
+});
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth,window.innerHeight);
 camera.position.setZ(30);
 
-renderer.render(scene,camera);
-
 const geometry = new THREE.TorusGeometry(10,3,16,100);
-const material = new THREE.MeshBasicMaterial({color:0xFF6347, wireframe:true});
+const material = new THREE.MeshBasicMaterial({color:0xFF6347, wireframe:false});
 const torus = new THREE.Mesh(geometry, material);
 
 const torus2 = new THREE.Mesh(geometry, material);
@@ -35,13 +34,14 @@ function moveCamera() {
   torus2.rotation.y += 0.0075;
   torus2.rotation.z += 0.005;
 
-// camera.position.z = t * -0.00001;
+  // camera.position.z = t * -0.00001;
   camera.position.x = t * 0.0002;
   camera.rotation.y = t * -0.0002;
 
+  renderer.render(scene, camera);
 }
 
-document.body.onscroll = moveCamera
+document.body.onscroll = moveCamera;
 
 function animate() {
   requestAnimationFrame(animate);
@@ -50,7 +50,6 @@ function animate() {
   torus.rotation.y += 0.0005;
   torus.rotation.z += 0.01;
   renderer.render(scene,camera);
-
 }
 
 animate();
