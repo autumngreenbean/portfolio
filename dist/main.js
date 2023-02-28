1269b39de0294125e41d52a6260b12576e539df3
Autumn Jing Green <67246658+autumngreenbean@users.noreply.github.com>
2023-02-28 10:14:31 -0800

    Changed from WebGL1 Renderer to WebGL Renderer
    Comments made by chatGPT based on recommended changes to the code. Attempting to render wireframe of object.

--- dist/main.js
+++ dist/main.js
@@ -4,10 +4,10 @@ import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1,1000);
 
-const renderer = new THREE.WebGL1Renderer({
-canvas: document.querySelector('#bg'),
-  
-})
+// Use WebGLRenderer instead of WebGL1Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth,window.innerHeight);
@@ -17,9 +17,12 @@ renderer.render(scene,camera);
 
 const geometry = new THREE.TorusGeometry(10,3,16,100);
 const material = new THREE.MeshBasicMaterial({color:0xFF6347, wireframe:true});
-const torus = new THREE.Mesh(geometry, material);
 
-const torus2 = new THREE.Mesh(geometry, material);
+// Create a new material instance for torus2
+const material2 = new THREE.MeshBasicMaterial({color:0xFF6347, wireframe:true});
+
+const torus = new THREE.Mesh(geometry, material);
+const torus2 = new THREE.Mesh(geometry, material2); // Use the new material instance for torus2
 
 torus.position.z = 30;
 torus.position.setX(-10);
@@ -35,10 +38,9 @@ function moveCamera() {
   torus2.rotation.y += 0.0075;
   torus2.rotation.z += 0.005;
 
-// camera.position.z = t * -0.00001;
+  // camera.position.z = t * -0.00001;
   camera.position.x = t * 0.0002;
   camera.rotation.y = t * -0.0002;
-
 }
 
 document.body.onscroll = moveCamera
@@ -51,7 +53,6 @@ function animate() {
   torus.rotation.y += 0.0005;
   torus.rotation.z += 0.01;
   renderer.render(scene,camera);
-
 }
 
 animate();
