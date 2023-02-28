bab86cf0061bbf8a810ea19b94428cbb9908dd85
Autumn Jing Green <67246658+autumngreenbean@users.noreply.github.com>
2023-02-28 10:26:13 -0800

    Revert "Update main.js"
    This reverts commit acbd30c5e117128c95a1582fbc4a306b6a18f1d9.

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ renderer.setSize(window.innerWidth,window.innerHeight);
 camera.position.setZ(30);
 
 const geometry = new THREE.TorusGeometry(10,3,16,100);
-const material = new THREE.MeshBasicMaterial({color:0xFF6347, wireframe:true});
+const material = new THREE.MeshBasicMaterial({color:0xFF6347, wireframe:false});
 const torus = new THREE.Mesh(geometry, material);
 
 const torus2 = new THREE.Mesh(geometry, material);
